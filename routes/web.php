ad42437de26cab3a7020cfe74765d2a6a6ade165
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2023-11-04 12:36:04 +0300

    Added Laravel UI

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+require __DIR__.'/auth.php';
+
+Auth::routes();
